3b1d8ac24a765fc918c2dba8ca0e76a926feca4f
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2024-10-18 13:51:47 +0300

    Create Switch.dart

---
+++ Switch.dart
@@ -0,0 +1,73 @@
+// Copyright 2019 the Dart project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorSchemeSeed: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  const MyHomePage({
+    super.key,
+    required this.title,
+  });
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+   bool isON = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('OFF'),
+            
+            Switch(
+            value: isON,
+            activeColor: Colors.red,
+            onChanged: (bool value) {
+              // This is called when the user toggles the switch.
+              setState(() {
+                isON = value;
+              });
+            },
+          ),
+            
+          Text('ON')  //add a bit more UI
+            
+           
+          ],
+        ),
+      ),
+     
+      
+    );
+  }
+}
