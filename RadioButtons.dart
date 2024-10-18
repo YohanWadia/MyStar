ae68b8278980516ff07133b122747018baedffe3
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2024-10-18 23:06:45 +0300

    Create RadioButtons.dart

---
+++ RadioButtons.dart
@@ -0,0 +1,112 @@
+// Copyright 2019 the Dart project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorSchemeSeed: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  const MyHomePage({
+    super.key,
+    required this.title,
+  });
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int selectedRadio=1;
+  
+  
+setSelectedRadio(int val) {
+  setState(() {
+    selectedRadio = val;
+  });
+}
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            
+      Expanded(child:
+        ListTile(
+        title: Text("Male"),
+        leading: Radio(
+          value: 1,
+          groupValue: selectedRadio,
+          activeColor: Colors.green,
+          onChanged: (val) {
+            print("Radio $val");
+            setSelectedRadio(val!);
+          },
+        ),
+      ),
+    ),
+    
+      Expanded(child:
+        ListTile(
+        title: Text("Female"),
+        leading: Radio(
+          value: 2,
+          groupValue: selectedRadio,
+          activeColor: Colors.green,
+          onChanged: (val) {
+            print("Radio $val");
+            setSelectedRadio(val!);
+          },
+        ),
+      ),
+    ),
+    
+      Expanded(child:
+        ListTile(
+        title: Text("EMail"),
+        leading: Radio(
+          value: 3,
+          groupValue: selectedRadio,
+          activeColor: Colors.green,
+          onChanged: (val) {
+            print("Radio $val");
+            setSelectedRadio(val!);
+          },
+        ),
+      ),
+    ),
+    
+            
+            
+           
+          ],
+        ),
+      ),
+      
+    );
+  }
+}
