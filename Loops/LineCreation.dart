5c92e7637dcc65539e11aabd8cdcd8cb5e2ef277
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2024-10-22 21:33:13 +0300

    Create LineCreation.dart

---
+++ Loops/LineCreation.dart
@@ -0,0 +1,20 @@
+Future<void> main() async {
+  
+  String line = "";
+  
+  
+  for (int i = 1; i <= 10; i++) {
+
+      line = line + "yohan, ";    
+    
+  }
+
+  print(line);
+
+
+//Question
+//what is i want the final result to look like
+//yohan, yohan, yohan, yohan, yohan, yohan, yohan, yohan, yohan and yohan.
+
+
+}
