f6273aa52b5bc7469792708cffdf896004a7eed9
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2024-10-22 21:13:07 +0300

    Create totaling.dart

---
+++ Loops/totaling.dart
@@ -0,0 +1,21 @@
+Future<void> main() async {
+  
+  
+  
+  
+  for (int i = 1; i <= 10; i++) {
+    print("Value of i $i");
+
+    
+    
+  }
+
+  print("the total of the first 10 numbers is ");
+
+
+
+
+
+
+
+}
