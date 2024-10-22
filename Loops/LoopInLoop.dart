6dce05e7306464dc7dbe65f404ee000b2f081281
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2024-10-22 13:58:40 +0300

    Create LoopInLoop.dart

---
+++ Loops/LoopInLoop.dart
@@ -0,0 +1,18 @@
+Future<void> main() async {
+//Nested Loops or Loop in Loop
+
+
+  
+  for (int i = 1; i <= 10; i++) {
+    print("i ================= $i");
+
+    for(int j =1; j<=10; j++){
+
+      print("j: $j");
+
+    }//j loop ends    
+
+  }//i loop ends
+
+
+}
