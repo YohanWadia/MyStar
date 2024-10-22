61e3e2ddf2c9bac39ec5ed47313b68fba260cf0c
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2024-10-22 22:48:48 +0300

    Create Stars.dart

---
+++ Loops/Stars.dart
@@ -0,0 +1,25 @@
+Future<void> main() async {
+  
+  
+  
+  
+  for (int i = 1; i <= 10; i++) {
+    String line = "";
+      
+      for(int j =1; j<=10; j++){
+
+          line = line  + "*";
+        
+      }//j 
+
+
+    print(line);
+  }//i 
+
+  
+
+
+
+
+
+}
