93e4389942c5ea7b072a43ff4b8530bad53c48a2
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2024-10-22 12:13:14 +0300

    Create LoopDoWhile.dart

---
+++ LoopDoWhile.dart
@@ -0,0 +1,22 @@
+Future<void> main() async {
+ 
+  int i = 0;
+  
+  do{
+    print('hello.....$i');
+    i++;
+  }while(i<10);
+  
+  print("---------------------");
+  //caution
+
+  // int x = 88;
+
+  // do{
+  //   print('hello.....$x');
+  //   x++;
+  // }while(x>100);
+
+
+  
+}
