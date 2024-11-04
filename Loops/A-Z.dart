5fbec0763a3141c03db9eba34ca0e381e9857950
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2024-11-04 23:31:50 +0200

    Create A-Z.dart

---
+++ Loops/A-Z.dart
@@ -0,0 +1,12 @@
+// https://www.ascii-code.com/
+Future<void> main() async {
+  for (int i = 65; i <= 90; i++) {  
+    
+    print(String.fromCharCode(i));
+
+  }
+  
+}
+
+// can you print out  a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,
+// and z,y,x,w,v,u,t,s,r,q,p,o,n,m,l,k,j,i,h,g,f,e,d,c,b,a,
