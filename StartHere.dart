23b4cfcba5e2ed1882d37f345c5e8696073f8c34
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2024-12-02 11:20:59 +0200

    Update StartHere.dart

--- StartHere.dart
+++ StartHere.dart
@@ -1,60 +1,55 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return MaterialApp(     
+      home: const MyHomePage(title: 'Flutter Example App'),
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorSchemeSeed: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  final String title;
+  const MyHomePage({super.key, required this.title});
 
-  const MyHomePage({
-    super.key,
-    required this.title,
-  });
+  final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  
+class _MyHomePageState extends State<MyHomePage> { 
+
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text("|--Start Here--|"),  
-          
-          ElevatedButton(
-          child:Text("Click"),
-          onPressed:(){ },
+      body: Center(
+        child: Column(          
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            ElevatedButton(
+              child: Text("ClickME"),
+              onPressed: (){},
+            )
+            
+          ],
         ),
-
-        
-          
-          
-          
-        ],
       ),
-     
     );
   }
 }
