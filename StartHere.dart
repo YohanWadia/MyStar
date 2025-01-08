fd4d9eee2c80439b71f3c94ef0c588ddba59eeb4
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2025-01-08 10:30:48 +0200

    Update StartHere.dart

--- StartHere.dart
+++ StartHere.dart
@@ -7,10 +7,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(     
+    return MaterialApp(
+      title: 'Flutter App!!',
+      theme: ThemeData(
+        colorSchemeSeed: Colors.indigo,
+        useMaterial3: true,
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        colorSchemeSeed: Colors.blue,
+        useMaterial3: true,
+        brightness: Brightness.dark,
+      ),
       home: const MyHomePage(title: 'Flutter Example App'),
       debugShowCheckedModeBanner: false,
     );
@@ -26,30 +36,23 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> { 
-
+class _MyHomePageState extends State<MyHomePage> {
+  
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(          
+      body: Center(        
+        child: Column(         
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            ElevatedButton(
-              child: Text("ClickME"),
-              onPressed: (){},
-            )
             
           ],
         ),
       ),
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
