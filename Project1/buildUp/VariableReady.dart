3b06bc68db6d271a8a3af51745255157f9dd75ed
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2025-01-16 12:26:28 +0200

    Update VariableReady.dart

--- Project1/buildUp/VariableReady.dart
+++ Project1/buildUp/VariableReady.dart
@@ -39,7 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
   
   String b1="",b2="",b3="",b4="",b5="",b6="",b7="",b8="",b9="";
   int score1=0,score2=0;
-  int move=1;
+  int move=0;
   String whoseChance="Player1";
   String toPut="O";
 
