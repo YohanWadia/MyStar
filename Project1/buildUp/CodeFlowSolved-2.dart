22158b9c0f75ec85b0de542612314328fc20788c
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2025-01-17 22:40:17 +0200

    Update CodeFlowSolved-2.dart

--- Project1/buildUp/CodeFlowSolved-2.dart
+++ Project1/buildUp/CodeFlowSolved-2.dart
@@ -40,8 +40,10 @@ class _MyHomePageState extends State<MyHomePage> {
   String b1="",b2="",b3="",b4="",b5="",b6="",b7="",b8="",b9="";
   int score1=0,score2=0;
   int move=0;
-  String whoseChance="Player1";
-  String toPut = "O";
+  String name1="Vejas", name2="Yohan";
+  String whoseChance="Vejas";
+  String info = "to play";
+  String toPut="O";
 
 
   @override
@@ -214,9 +216,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
 
           // ------put the 3 Texts below-------
-          Text("$whoseChance to play...", style: TextStyle(fontSize: 36),),
-          Text("Player1: $score1"),
-          Text("Player2: $score2")
+          Text("$whoseChance $info", style: TextStyle(fontSize: 24),),
+          Text("$name1: $score1"),
+          Text("$name2: $score2")
 
 
         ],
@@ -234,11 +236,11 @@ class _MyHomePageState extends State<MyHomePage> {
       toPut="O";
     }
 
-    if(whoseChance=="Player1"){
-      whoseChance="Player2";
+    if(whoseChance==name1){
+      whoseChance=name2;
     }
-    else if(whoseChance=="Player2"){
-      whoseChance="Player1";
+    else if(whoseChance==name2){
+      whoseChance=name1;
     }
 
   }
