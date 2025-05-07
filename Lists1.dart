5b290b60cd4a63a8de8816c4692e1e8e9cc48c21
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2025-05-07 12:48:16 +0300

    Create Lists1.dart

---
+++ Lists1.dart
@@ -0,0 +1,44 @@
+Future<void> main() async {
+  
+  List<int> marks = [12,8,44,13,9,23,35,62,77,55];
+
+  List<String> names = ["Aug","Greta","Vilus","Olivia","Linas","Jekubas","Vytis","Rokas","Herkus","Rokas"];
+
+
+  for(int i=0; i<10;i++){
+    
+    print("at index $i is ");
+
+
+  }
+
+
+  //add
+  for(int i=0; i<10;i++){ 
+
+    //print("at index $i is ");
+  }
+
+
+  //remove
+  //marks.remove()
+  //marks.removeAt()
+  for(int i=0; i<10;i++){    
+
+    //print("at index $i is ");
+  }
+
+  //manupilate
+  //remove
+  for(int i=0; i<10;i++){    
+
+ //print("at index $i is ");
+  }
+
+
+
+
+
+
+
+}
