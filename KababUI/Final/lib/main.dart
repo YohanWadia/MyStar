6252d4cc5743c8a13fd1f367e8f0ac3cd4f42223
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2025-06-04 12:27:53 +0300

    Update main.dart

--- KababUI/Final/lib/main.dart
+++ KababUI/Final/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(const ScaleTransitionExampleApp());
@@ -63,15 +65,22 @@ var kababasList = [
   'Open Kababas'
 ];
 
+int selectedRadio=1;
+String meatSelected="Male";
+
+
 List<bool> checked = [false,false,false,false];
 
-int selectedRadio=0;
-
-List<bool> choice =[false,false,false];
+List<bool> choice =[false,false,false,false];
 
 double sliderValue = 3;
 
 bool isON = true;
+
+String status = "";
+String buzzer = "";
+String waitingTime = "";
+ 
 
 
 //=======================Variables
@@ -100,28 +109,83 @@ bool isON = true;
               decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(15),
               color: Colors.pink[200]),
-            child: DropdownButton(
-            style: TextStyle(color: Colors.black, fontSize: 16),
-            dropdownColor: Colors.pink[200],
-            focusColor: Colors.pink[100],
-            icon: Icon(Icons.arrow_downward),
-            value: presentValue,
-            items: kababasList.map((String items) {
-              return DropdownMenuItem(value: items, child: Text(items));
-            }).toList(),
-            onChanged: (String? newValue) {
-              //how to disallow "Select Kababas" from being selected
-                setState(() {
-                  presentValue = newValue!;
-                  print("You have selected $presentValue");
-                });
-              
-            },
-        ),
+                child: DropdownButton(
+                style: TextStyle(color: Colors.black, fontSize: 16),
+                dropdownColor: Colors.pink[200],
+                focusColor: Colors.pink[100],
+                icon: Icon(Icons.arrow_downward),
+                value: presentValue,
+                items: kababasList.map((String items) {
+                  return DropdownMenuItem(value: items, child: Text(items));
+                }).toList(),
+                onChanged: (String? newValue) {
+                  //how to disallow "Select Kababas" from being selected
+                    setState(() {
+                      presentValue = newValue!;
+                      print("You have selected $presentValue");
+                    });
+                 
+                },
+            ),
           ),
         //=============================
+        SizedBox(height: 30,),
+        Text("Meat",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+        Row(
+          children: [            
+          Expanded(child:
+            ListTile(
+            title: Text("Male"),
+            leading: Radio(
+              value: 1,
+              groupValue: selectedRadio,
+              activeColor: Colors.green,
+              onChanged: (val) {
+              print("RadioButton $val selected");
+                selectedRadio = val!;
+                meatSelected="Male";
+                setState(() {});
+              },
+            ),
+          ),
+        ),
+   
+          Expanded(child:
+            ListTile(
+            title: Text("Female"),
+            leading: Radio(
+              value: 2,
+              groupValue: selectedRadio,
+              activeColor: Colors.green,
+              onChanged: (val) {
+                print("RadioButton $val selected");
+                selectedRadio = val!;
+                setState(() {});
+              }
+            ),
+          ),
+        ),
+   
+          Expanded(child:
+            ListTile(
+            title: Text("EMail"),
+            leading: Radio(
+              value: 3,
+              groupValue: selectedRadio,
+              activeColor: Colors.green,
+              onChanged: (val) {
+                print("RadioButton $val selected");
+                selectedRadio = val!;
+                setState(() {});
+              },
+            ),
+          ),
+        ),
+    ]),
+//==============================
 
-        Text("Extras"),
+        SizedBox(height: 30,),
+        Text("Extras",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
         Row(          
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -181,62 +245,8 @@ bool isON = true;
         ),
         //=============================
 
-        Text("Meat"),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            
-          Expanded(child:
-            ListTile(
-            title: Text("Male"),
-            leading: Radio(
-              value: 1,
-              groupValue: selectedRadio,
-              activeColor: Colors.green,
-              onChanged: (val) {
-              print("RadioButton $val selected");
-                selectedRadio = val!;
-                setState(() {});
-              },
-            ),
-          ),
-        ),
-    
-          Expanded(child:
-            ListTile(
-            title: Text("Female"),
-            leading: Radio(
-              value: 2,
-              groupValue: selectedRadio,
-              activeColor: Colors.green,
-              onChanged: (val) {
-                print("RadioButton $val selected");
-                selectedRadio = val!;
-                setState(() {});
-              }
-            ),
-          ),
-        ),
-    
-          Expanded(child:
-            ListTile(
-            title: Text("EMail"),
-            leading: Radio(
-              value: 3,
-              groupValue: selectedRadio,
-              activeColor: Colors.green,
-              onChanged: (val) {
-                print("RadioButton $val selected");
-                selectedRadio = val!;
-                setState(() {});
-              },
-            ),
-          ),
-        ),
-    ]),
-//==============================
-
-      Text("Padazai"),
+      SizedBox(height: 30,),
+      Text("Padazai",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
       Row(          
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -268,11 +278,22 @@ bool isON = true;
                       });
                     },
                   ),
+
+                  ChoiceChip(
+                    label: Text('Judo'),
+                    selected: choice[3] == true,
+                    onSelected: (bool selected) {
+                      setState(() {
+                        choice[3] = selected ? true : false;
+                      });
+                    },
+                  ),
           ],
         ),
 //==================================
 
-        Text("Spice"),
+        SizedBox(height: 30,),
+        Text("Spice",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
         Slider(
           value: sliderValue,
           max: 5,
@@ -286,15 +307,16 @@ bool isON = true;
           },
         ),
 //====================================
-
-Row(
+      SizedBox(height: 30,),
+      Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text('OFF'),
-            
+           
             Switch(
             value: isON,
             activeColor: Colors.red,
+            inactiveThumbColor: Colors.green,
             onChanged: (bool value) {              
               setState(() {
                 isON = value;
@@ -303,12 +325,16 @@ Row(
 
             },
           ),
-            
+           
           Text('ON')  //add a bit more UI
-            
+           
            
           ],
         ),
+        //=================================
+
+        SizedBox(height: 20,),
+        Text("$status$buzzer$waitingTime",style: TextStyle(fontSize: 18),),
 
 
 
@@ -319,10 +345,54 @@ Row(
 
 
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: placeMyOrder,
         tooltip: 'Place Order',
         child: const Icon(Icons.menu),
       ),
     );
   }
+
+  void placeMyOrder() {
+    String fullOrder="";
+
+
+
+    //====================
+            showDialog<String>(
+                  context: context,
+                  builder: (BuildContext context) => AlertDialog(
+                    title: const Text('Order'),
+                    content: const Text('You want a Pizza'),
+                    actions: <Widget>[
+                      //===========Cancelled
+                      TextButton(
+                        onPressed: () {
+                          status = "Order Cancelled. ";
+                          Navigator.pop(context);
+                          setState(() { });
+                        },
+                        child: const Text('Cancel'),
+                      ),
+
+                      //=================OK
+                      TextButton(
+                        onPressed: () {
+                          status = "Order Confirmed. ";
+                          Random rnd = Random();
+                          int number = rnd.nextInt(15);
+                          buzzer="Please take buzzer# $number. ";
+                          number = number + 5;
+                          waitingTime = "Ready in $number mins.";
+                          Navigator.pop(context);
+                          setState(() { });
+                        },
+                        child: const Text('OK'),
+                      ),
+                    ],
+                    )
+                  );
+
+  }
 }
+
+
