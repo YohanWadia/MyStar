2fc0b7db828787cf8516a20904fb072e2eb5d4a4
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2026-03-01 21:40:28 +0200

    Update Switch.dart

--- KababUI/Switch.dart
+++ KababUI/Switch.dart
@@ -1,9 +1,4 @@
-// Copyright 2019 the Dart project authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license
-// that can be found in the LICENSE file.
-
 import 'package:flutter/material.dart';
-
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
